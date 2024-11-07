a45572581f1ee621779eec4acdd9dbe33b8c7717
hansen1416 <badapplesweetie@gmail.com>
2024-11-07 15:31:59 +0800

    1

--- download_smplify.sh
+++ download_smplify.sh
@@ -6,8 +6,9 @@ read -p "Password (SMPLify):" password
 username=$(urle $username)
 password=$(urle $password)
 
+mkdir -p data/smpl/
 wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=smplify&resume=1&sfile=mpips_smplify_public_v2.zip' -O './data/smpl/smplify.zip' --no-check-certificate --continue
-unzip data/smpl/smplify.zip -d data/smpl/smplify
-mv data/smpl/smplify/smplify_public/code/models/basicModel_neutral_lbs_10_207_0_v1.0.0.pkl data/basicModel_neutral_lbs_10_207_0_v1.0.0.pkl
+unzip data/smpl/smplify.zip -d data/smpl/
+mv data/smpl/smplify_public/code/models/basicModel_neutral_lbs_10_207_0_v1.0.0.pkl data/basicModel_neutral_lbs_10_207_0_v1.0.0.pkl
 rm -rf data/smpl/smplify
 rm -rf data/smpl/smplify.zip
