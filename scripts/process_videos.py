7e6b4b7adf56a6dd2f19d66f79e1c3bde3372242
hansen1416 <badapplesweetie@gmail.com>
2024-11-19 15:26:31 +0800

    1

---
+++ scripts/process_videos.py
@@ -0,0 +1,28 @@
+import os
+import subprocess
+
+target_folder = os.path.join("/4D-Humans", "videos")
+results_folder = os.path.join("/4D-Humans", "output")
+
+# list all files in `target_folder`
+files = os.listdir(target_folder)
+
+print(f"got {len(files)} files")
+
+for f in files:
+
+    file_name = f.split(".")[0]
+
+    # check if the results already exists
+    if os.path.exists(os.path.join(results_folder, file_name)):
+        print(f"{f} already processed")
+        continue
+    else:
+        print(f"{f} in process")
+
+    video_path = os.path.join(target_folder, f)
+
+    # python track.py video.source="example_data/videos/gymnasts.mp4"
+    command = ["python", "track.py", f'video.source="{video_path}"']
+
+    subprocess.run(command, check=True)
